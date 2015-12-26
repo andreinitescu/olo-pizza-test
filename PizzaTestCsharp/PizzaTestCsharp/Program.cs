afdb3b89d722a42e5888b491520b827552a91f26
benharrison <bharrison912@gmail.com>
2015-12-25 21:57:12 -0500

    initial commit

---
+++ PizzaTestCsharp/PizzaTestCsharp/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PizzaTestCsharp
+{
+    class Topping
+    {
+        public string[] toppings { get; set; }
+    }
+
+    class ToppingCount
+    {
+        public Topping Topping { get; set; }
+        public int Count { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText("App_Data/pizzas.json");
+            var toppings = JsonConvert.DeserializeObject<Topping[]>(json);
+
+            var toppingCounts = new List<ToppingCount>();
+
+            foreach(var topping in toppings)
+            {
+                var found = false;
+
+                if (topping.toppings.Length > 1) Array.Sort(topping.toppings);
+
+                foreach(var toppingCount in toppingCounts)
+                {
+                    if (Enumerable.SequenceEqual(topping.toppings, toppingCount.Topping.toppings))
+                    {
+                        found = true;
+                        toppingCount.Count++;
+                        continue;
+                    }
+                }
+
+                if (!found)
+                {
+                    toppingCounts.Add(new ToppingCount { 
+                        Topping = topping,
+                        Count = 1
+                    });
+                }
+            }
+
+            foreach(var toppingCount in toppingCounts.OrderBy(x => x.Count).Reverse().Take(20))
+            {
+                var sb = new StringBuilder();
+                foreach (var topping in toppingCount.Topping.toppings)
+                    sb.AppendFormat("{0}, ", topping);
+
+                Console.WriteLine("{0}\t{1}", toppingCount.Count, sb.ToString().Trim().TrimEnd(','));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pizza-test
+
+Code Test: 
+
+Finding Popular Build-Your-Own Pizzas 
+
+A pizza chain wants to know which topping combinations are most popular for Build Your Own pizzas.
+
+Given the sample of orders at http://files.olo.com/pizzas.json, write an application (in C#, F# or JavaScript) to output the top 20 most frequently ordered pizza configurations, listing the toppings for each along with the number of times that pizza configuration has been ordered.
+
+Please spend no more than 30 minutes on this challenge. 
